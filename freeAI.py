1edf46a1a86c2570f4f89ee3e6858b6767d2bab3
HotDrify <105966726+HotDrify@users.noreply.github.com>
2023-07-04 10:36:43 +0300

    update freeAI.py

--- freeAI.py
+++ freeAI.py
@@ -69,7 +69,7 @@ class AIMod(loader.Module):
     @loader.unrestricted
     async def banChatcmd(self, message):
         chat = await message.get_chat()
-        if chat.id in self._db['banChats']:
+        if chat.id in self.get('banChats'):
             await utils.answer(
               message,
               self.strings('channel_err')
